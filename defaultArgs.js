65818e792255ae7a5ce21f30c52ebb52410ab56d
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-10 14:42:59 -0400

    Adding example to defaultArgs.js

--- defaultArgs.js
+++ defaultArgs.js
@@ -48,3 +48,52 @@ as the argument.
 */
 
 makeAjaxRequest('google.com', null);
+
+/*
+Let's do a more complicated example, where
+we have a function that returns a new User.
+A User has an id property, which the function
+must generate itself, and randomly. How do
+we do this?
+
+We create a function that returns a randomly
+generate number.
+*/
+
+function generateId() {
+	return Math.random() * 999999;
+}
+
+/*
+We pass in the random number generator as
+the default parameter. This is the User
+constructor.
+*/
+
+function User(id = generateId()) {
+	this.id = id;
+}
+
+/*
+This function creates an Admin User, given
+an already existing user.
+*/
+
+function createAdminUser(user) {
+	user.admin = true;
+  return user;
+}
+
+/*
+Because we are making use of default
+arguments for generating IDs, our code
+for creating Users is much cleaner.
+*/
+createAdminUser(new User());
+
+/*
+Now, if we have some specific user ID in
+mind, we can specify it and override the
+randomly generated one.
+*/
+new User(1);
