49907b75df2a0768b70687ef26c9df91619f7518
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-15 16:13:39 -0400

    Adding more examples to generators.js

--- generators.js
+++ generators.js
@@ -33,4 +33,42 @@ It will become apparent soon enough that
 for-of loops are actually closely related to
 generators. So we'll explore generators a bit
 and see how it all ties together.
+
+Generators are not easy to learn, so we'll
+learn about it from the ground up: what it is,
+what it does, how we use it, and so on. First,
+let's define what it is.
+
+A generator is a function that can be entered
+and exited multiple times. Usually, a function
+is called and then exited... and then it is
+done with.
 */
+
+/*
+Let's talk syntax. To turn a function into a
+generator, we add an asterisk '*' right after
+the function keyword. We can also put the
+asterisk right before the function name.
+
+function* myGenerator() { ... }
+function *myGenerator() { ... }
+*/
+
+function* numbers() {
+  /*
+  The yield keyword is special and particular
+  for generators.
+  */
+  yield;
+}
+
+/*
+This is what happens when we call numbers()
+and then use .next() multiple times. This is
+strange, but we'll make it clear later on.
+*/
+
+const gen = numbers();
+gen.next(); // { "done": false }
+gen.next(); // { "done": true }
