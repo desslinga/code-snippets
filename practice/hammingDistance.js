ebece120db7377fe4850692c6817bb0825264020
desslinga <fides.linga@mail.utoronto.ca>
2019-05-20 21:02:22 -0400

    Adding hamming strings solution, deleting top level files.

--- practice/hammingDistance.js
+++ practice/hammingDistance.js
@@ -2,13 +2,19 @@ let hammingDistance = function(x, y) {
     const bitX = x.toString(2);
     const bitY = y.toString(2);
     const bitLarger = (x >= y ? bitX : bitY);
+    let bitSmaller = (x >= y ? bitY : bitX);
+    const bitDiff = Math.abs(bitLarger.length - bitSmaller.length);
+    if (bitDiff != 0) bitSmaller = ("0".repeat(bitDiff)).concat(bitSmaller);
     let sum = 0;
-    for (let i = bitLarger.length - 1; i >= 0; i--) {
-      sum += (bitX[i] != bitY[i]);
-      console.log(bitX[i], bitY[i], "ok", bitX[i] != bitY[i], sum);
+    for (let i = 0; i < bitLarger.length; i++) {
+      sum += bitLarger[i] !== bitSmaller[i];
     }
-    console.log(bitX, bitY, sum);
     return sum;
 };
 
-hammingDistance(14,4);
+let hammingDistanceAlt = function(x, y) {
+  return (x ^ y)
+    .toString(2)
+    .split('')
+    .reduce((sum, curr) => sum += curr === '1', 0);
+}
